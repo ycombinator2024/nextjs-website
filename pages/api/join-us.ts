9e67e55d94c53ec514985b44a9b0eaeda2f7e27d
Nikita Malinovsky <nikita@malinovsky.net>
2023-10-19 20:15:06 -0700

    .

--- pages/api/join-us.ts
+++ pages/api/join-us.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z, ZodError } from "zod";
 import prisma from "@/lib/client";
+import Mailgun from "mailgun-js";
 
 const schema = z.object({
   name: z.string(),
@@ -19,20 +20,45 @@ export default async function handler(
   }
 
   try {
-    const event = schema.parse(JSON.parse(req.body));
+    const { name, email, phone, childrenInfo, subscribe } = schema.parse(
+      JSON.parse(req.body)
+    );
 
     await prisma.joined.create({
       data: {
-        name: event.name,
-        email: event.email,
-        phone: event.phone,
-        children_info: event.childrenInfo,
-        subscribe: event.subscribe,
+        name: name,
+        email: email,
+        phone: phone,
+        children_info: childrenInfo,
+        subscribe: subscribe,
       },
     });
 
+    const mg = Mailgun({
+      apiKey: "568c6115109b21b2d48075a124479d7b-3750a53b-d0b5a882",
+      domain: "sandbox5f2efa7b5e7f45dba21598af50835d06.mailgun.org",
+    });
+
+    let isSubed = subscribe ? "Yes" : "No";
+
+    const data = {
+      from: "Razvedchik.org <razvedchik.dnn@gmail.com>",
+      to: ["niki@malinovsky.net"],
+      subject: name + " joined.",
+      "h:X-Mailgun-Variables": JSON.stringify({
+        email: email,
+        phone: phone,
+        childrenInfo: childrenInfo,
+        subscribe: isSubed,
+      }),
+      template: "joined",
+    };
+
+    mg.messages().send(data);
+
     res.status(200).json({ message: "Success" });
   } catch (err) {
+    console.log(err);
     if (err instanceof ZodError) {
       res.status(400).json({ message: err.issues[0].message });
     } else {
